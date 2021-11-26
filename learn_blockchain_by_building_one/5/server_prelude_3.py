ceb182f3f15f97d58a869d3d219db5d816cf71fa
Lynn Bradshaw <readyready15728@gmail.com>
2021-11-26 15:17:16 +0000

    Adding server_prelude_3.py

---
+++ learn_blockchain_by_building_one/5/server_prelude_3.py
@@ -0,0 +1,76 @@
+import asyncio
+import textwrap
+
+class ConnectionPool:
+    def __init__(self):
+        self.connection_pool = set()
+
+    def send_welcome_message(self, writer):
+        """Sends a welcome message to a newly connected client"""
+
+        message = textwrap.dedent(f"""
+        ===
+        Welcome {writer.nickname}!
+
+        There are {len(self.connection_pool) - 1} user(s) here besides you.
+        """)
+
+        writer.write(f'{message}\n'.encode())
+
+    def broadcast(self, writer, message):
+        """Broadcasts a general message to the entire pool"""
+
+        pass
+
+    def broadcast_user_join(self, writer):
+        """Calls the broadcast method with a "user joining" message"""
+
+        pass
+
+    def broadcast_user_quit(self, writer):
+        """Calls the broadcast method with a "user quitting" message"""
+
+        pass
+
+    def broadcast_new_message(self, writer, message):
+        """Calls the broadcast method with a user's chat message"""
+
+        pass
+
+    def list_users(self, writer):
+        """Lists all the users in the pool"""
+
+        pass
+
+    def add_new_user_to_pool(self, writer):
+        """Adds a new user to our existing pool"""
+
+        self.connection_pool.add(writer)
+
+    def remove_user_from_pool(self, writer):
+        """Removes an existing user from our pool"""
+
+        self.connection_pool.remove(writer)
+
+async def handle_connection(reader, writer):
+    # Get a nickname for the new client
+    writer.write('> Choose your nickname: '.encode())
+    response = await reader.readuntil(b'\n')
+    writer.nickname = response.decode().strip()
+
+    connection_pool.add_new_user_to_pool(writer)
+    connection_pool.send_welcome_message(writer)
+    await writer.drain()
+
+    # Let's close the connection and clean up
+    writer.close()
+    await writer.wait_closed()
+
+async def main():
+    server = await asyncio.start_server(handle_connection, '0.0.0.0', 8888)
+
+    async with server:
+        await server.serve_forever()
+
+connection_pool = ConnectionPool()
+asyncio.run(main())
