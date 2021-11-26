d9bc6684f8e1a0d4fadf7b395eb0189db6a4f22c
Lynn Bradshaw <readyready15728@gmail.com>
2021-11-26 10:17:58 +0000

    Adding partial chapter 4 code

---
+++ learn_blockchain_by_building_one/full_blockchain.py
@@ -0,0 +1,48 @@
+import datetime
+import hashlib
+import json
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+
+        # Create the genesis block
+        print('Creating genesis block')
+        self.new_block()
+
+    def new_block(self, previous_hash=None):
+        block = {
+            'index': len(self.chain),
+            'timestamp': datetime.utcnow().isoformat(),
+            'transactions': self.pending_transactions,
+            'previous_hash', previous_hash,
+            'nonce': None
+        }
+
+        # Get the hash of this new block and add it to the block
+        block['hash'] = self.hash(block)
+
+        # Reset the list of pending transactions
+        self.pending_transactions = []
+        # Add the block to the chain
+        self.chain.append(block)
+
+        print(f'Created block {block['index']}')
+        return block
+
+    @staticmethod
+    def hash(block):
+        # Ensure the dictionary is sorted or hashes will be inconsistent
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def last_block(self):
+        # Returns the last block in the chain (if there are blocks)
+        return self.chain[-1] if self.chain else None
+
+    def proof_of_work(self):
+        pass
+
+    def valid_hash(self):
+        pass
