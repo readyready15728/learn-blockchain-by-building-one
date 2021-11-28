9a6e21feb787a1ca27464932439ee13905064822
Lynn Bradshaw <readyready15728@gmail.com>
2021-11-28 15:55:57 +0000

    Extending signature.py

--- learn_blockchain_by_building_one/6/signature.py
+++ learn_blockchain_by_building_one/6/signature.py
@@ -1,5 +1,5 @@
 from nacl.encoding import HexEncoder
-from nacl.signing import SigningKey
+from nacl.signing import SigningKey, VerifyKey
 
 # Generate a new random private key for Bob (we call this a signing key)
 bobs_private_key = SigningKey.generate()
@@ -13,4 +13,10 @@ bobs_public_key = bobs_private_key.verify_key
 # Serialize the verify key to send it to a third party
 bobs_public_key_hex = bobs_public_key.encode(encoder=HexEncoder)
 
-print(bobs_public_key_hex.decode('utf-8'))
+# Now, sign a message with Bob's private key
+signed_message = bobs_private_key.sign(b'Send $37 to Alice')
+print('Signed message:', signed_message)
+
+# And verify the key
+verify_key = VerifyKey(bobs_public_key_hex, encoder=HexEncoder)
+print('Verification attempt:', verify_key.verify(signed_message))
